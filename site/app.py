c9cdb660058b77b108182bcc228b95e594ceaa3b
Fox Wilson <foxwilson123@gmail.com>
2016-07-02 22:07:59 -0400

    Timeline (#13)
    * add reports module
    
    * Update about.html
    
    * Add files via upload
    
    * Update base.html
    
    * send emails with tracebacks to a defined SITE_ADMIN
    
    * fix production errors
    
    * really fix prod errors
    
    * Revert "really fix prod errors"
    
    This reverts commit 57328fe76188f36241a3139c8eec0c015d8ba05c.
    
    * really maybe fix
    
    * fix utils again
    
    * add more info to error emails
    
    * About/social media links
    
    * Update about.html
    
    * Add files via upload
    
    * Update base.html
    
    * no link decoration for social media
    
    * Add files via upload
    
    * Update about.html
    
    * fixes #5, use css/pure grids instead
    
    * Update base.css
    
    * Update base.css (#6)
    
    * Update base.css
    
    * Update base.html
    
    * Update base.css (#7)
    
    * Update base.html (#8)
    
    * HTML5 GET! (#9)
    
    * HTML compliance
    
    * Add alt-text
    
    * optipng -o7 -zm1-9
    
    * Update base.html
    
    * Update base.css
    
    * Update base.css
    
    * Update base.css
    
    * timeline.html
    
    * Update base.html
    
    * Update base.css
    
    * Update base.css
    
    * Update base.css
    
    * timeline.html
    
    * fix
    
    * add import volunteers script

--- site/app.py
+++ site/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash, redirect, make_response
 from database import database
+from utils import send_email, send_error_email
 import traceback
 
 import logging
@@ -53,6 +54,10 @@ def create_app(environment):
     @app.errorhandler(500)
     def internal_error(exc):
         trace = traceback.format_exc()
+        try:
+            send_error_email(environment, trace)
+        except:
+            trace = traceback.format_exc()
         return make_response(render_template("whoops.html", trace=trace), 500)
 
     return app
